f18d0184f12121e5efe1210dde9fd74be6516c10
Severuna <oxxxyfomenkova@gmail.com>
2023-05-18 22:13:03 +0300

    init repo

---
+++ src/Main.js
@@ -0,0 +1,12 @@
+
+import './Main.css';
+
+const Main = () => {
+    return (
+        <div className="main column">
+            <h1 className='title'>Carousel</h1>
+        </div>
+    )
+}
+
+export default Main;
