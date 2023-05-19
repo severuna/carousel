fc54ad05e28c282efb3af29124152411b7907d2c
Severuna <oxxxyfomenkova@gmail.com>
2023-05-19 22:40:39 +0300

    set simple infinity slider

--- src/Main.js
+++ src/Main.js
@@ -1,40 +1,78 @@
+import { useState, useEffect } from 'react';
 import './Main.css';
-import Card from './components/Card/Card';
+import Card from './Card/Card';
 
-const cards = [
-    { 
-        id: 1,
-        title: 'Card #'
-    },
-    { 
-        id: 2,
-        title: 'Card #'
-    },
-    { 
-        id: 3,
-        title: 'Card #'
-    },
-    { 
-        id: 4,
-        title: 'Card #'
-    },
-    { 
-        id: 5,
-        title: 'Card #'
-    },
-];
+const Main = () => {   
+    
+    const cards = [
+        { 
+            id: 1,
+            title: 'Card #'
+        },
+        { 
+            id: 2,
+            title: 'Card #'
+        },
+        { 
+            id: 3,
+            title: 'Card #'
+        },
+        { 
+            id: 4,
+            title: 'Card #'
+        },
+        { 
+            id: 5,
+            title: 'Card #'
+        },
+    ];
+    
 
-let elements = [];
+    let [ currentIndex, setCurrentIndex ] = useState(0);
 
-cards.forEach(element => {
-    elements.push(<Card {...element} />)
-})
+    const prevSlide = ( e ) => {
 
-const Main = () => {
+        e.preventDefault();
+
+        let counter = currentIndex - 1;
+        
+        if (counter === -1) {
+            return setCurrentIndex(4)
+        } else {
+            return setCurrentIndex(counter)
+        }
+
+    };
+
+    const nextSlide = ( e ) => {
+        e.preventDefault();
+
+        let counter = currentIndex + 1;
+
+        if (counter === cards.length ) {
+            setCurrentIndex(0)
+        } else {
+            setCurrentIndex(counter)
+        }
+
+    };
+
+    useEffect(() => {
+        setCurrentIndex(currentIndex)
+    }, [currentIndex])
+
     return (
         <div className="main column">
             <h1 className='title'>Carousel</h1>
-            {elements}
+            <div className='carousel row'>
+                <button onClick={(e) => prevSlide(e)} className='carousel-btn'>
+                    {'<'}
+                </button>
+                <Card {...cards[currentIndex]} />
+                <button onClick={(e) => nextSlide(e)} className='carousel-btn'>
+                    {'>'}
+                </button>
+            </div>
         </div>
     )
 }
